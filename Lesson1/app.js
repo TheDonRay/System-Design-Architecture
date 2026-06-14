962393455b84cf75087048024d0363f985275045
Rayat Chowdhury <rayatchowdhury2005@gmail.com>
2026-06-13 23:00:52 -0400

    working on the backend

--- Lesson1/app.js
+++ Lesson1/app.js
@@ -4,11 +4,20 @@ const app = express();
 // import any routes here below 
 app.use(express.json()); 
 
+// initialize the data here as such 
+const userData = []; 
+
 app.get('/', (req, res) =>  {
     res.json({ 
         Server: 'Successfully Running'
     }); 
-});  
+});   
+
+app.get('/users', (req, res) => { 
+    res.json({ 
+        Data: userData
+    }); 
+});   
 
 export default app; 
 
